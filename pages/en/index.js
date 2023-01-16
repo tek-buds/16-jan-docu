ef26bd7b7ee92ba22c253a877092e93102996291
Reena <reena.singh@tekbuds.com>
2023-01-16 14:19:32 +0530

    index.js changed

--- pages/en/index.js
+++ pages/en/index.js
@@ -184,7 +184,7 @@ class Index extends React.Component {
       return (
         <div className="productShowcaseSection paddingBottom">
           <h2>Who is Using This?</h2>
-          <p>This project is used by all these people</p>
+          <p>This project is used by all these people - 16jan</p>
           <div className="logos">{showcase}</div>
           <div className="more-users">
             <a className="button" href={pageUrl('users.html')}>
